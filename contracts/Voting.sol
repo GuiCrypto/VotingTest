00f2f6f5f6b7e05e2fe363bc34a0a57fc3fd1e75
Guilhain Averlant <g.averlant@mailfence.com>
2023-02-26 17:00:09 +0100

    restore initial voting file

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity >= 0.8.17;
+pragma solidity 0.8.13;
 import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
-//import "@openzeppelin/contracts/access/Ownable.sol";
+
 
 contract Voting is Ownable {
 
